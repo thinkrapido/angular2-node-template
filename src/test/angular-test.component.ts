07b0f9894bedd6938fc695d1f55b2b65f1916843
Romeo Disca <romeo.disca@gmail.com>
2016-06-12 10:57:36 +0200

    created localization files

--- src/test/angular-test.component.ts
+++ src/test/angular-test.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  templateUrl: 'test/angular-test.component.html',
-  styleUrls: ['test/angular-test.component.css']
+  templateUrl: 'test/angular-test.view.html',
+  styleUrls: ['test/angular-test.css']
 })
 export class AngularTestAppComponent {
   title = 'angular-test works!';
